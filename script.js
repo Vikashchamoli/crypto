2dc906968b8913d5a08e9a77490cd1a1af3af844
Vikashchamoli <105197815+Vikashchamoli@users.noreply.github.com>
2023-11-30 18:26:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// Select relevant HTML elements
+const filterButtons = document.querySelectorAll("#filter-buttons button");
+const filterableCards = document.querySelectorAll("#filterable-cards .card");
+
+// Function to filter cards based on filter buttons
+const filterCards = (e) => {
+    document.querySelector("#filter-buttons .active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+        // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
+        if(card.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "All") {
+            return card.classList.replace("hide", "show");
+        }
+        card.classList.add("hide");
+    });
+}
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
+
+
+
+let copy = (textId) => {
+    //Selects the text in the <input> elemet
+    document.getElementById(textId).select();
+    //Copies the selected text to clipboard
+    document.execCommand("copy");
+  };
+
