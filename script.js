2efaff39a0e447b0a280c675639f6bb2ca3ae478
Vikashchamoli <105197815+Vikashchamoli@users.noreply.github.com>
2023-12-01 13:47:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -23,7 +23,50 @@ filterButtons.forEach(button => button.addEventListener("click", filterCards));
 let copy = (textId) => {
     //Selects the text in the <input> elemet
     document.getElementById(textId).select();
+    
     //Copies the selected text to clipboard
     document.execCommand("copy");
+    
   };
 
+  const btn = document.getElementById('copy1');
+
+// ✅ Change button text on click
+btn.addEventListener('click', function handleClick() {
+  btn.textContent = 'Copied';
+  btn2.textContent = 'Copy';
+  btn3.textContent = 'Copy';
+  btn4.textContent = 'Copy';
+});
+
+const btn2 = document.getElementById('copy2');
+
+// ✅ Change button text on click
+btn2.addEventListener('click', function handleClick() {
+  btn2.textContent = 'Copied';
+  btn.textContent = 'Copy';
+  btn3.textContent = 'Copy';
+  btn4.textContent = 'Copy';
+});
+
+const btn3 = document.getElementById('copy3');
+
+// ✅ Change button text on click
+btn3.addEventListener('click', function handleClick() {
+  btn3.textContent = 'Copied';
+  btn2.textContent = 'Copy';
+  btn.textContent = 'Copy';
+  btn4.textContent = 'Copy';
+});
+
+
+const btn4 = document.getElementById('copy4');
+
+// ✅ Change button text on click
+btn4.addEventListener('click', function handleClick() {
+  btn4.textContent = 'Copied';
+  btn3.textContent = 'Copy';
+  btn2.textContent = 'Copy';
+  btn.textContent = 'Copy';
+
+});
